7b062ce01eea0ed83034e9b5500cfe83cbaf1b65
unknown <40340425+davidcallanan@users.noreply.github.com>
2020-05-23 09:21:42 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # node-deno-integration
+
+ - `cd node_app`
+ - `deno bundle deno_integration/deps.ts deno_integration/deps.js`
+ - `node app.js`
---
+++ deno_shuffle/mod.ts
@@ -0,0 +1,14 @@
+export function shuffleArray<T>(arr: Array<T>) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let x = arr[i];
+    arr[i] = arr[j];
+    arr[j] = x;
+  }
+
+  return arr;
+}
+
+export function shuffleString(str: string): string {
+  return shuffleArray(str.split("")).join("");
+}
